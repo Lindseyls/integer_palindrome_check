5d5de7c9c80816cbf2fb46f24213bef5a4134c42
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-04-01 21:55:32 -0700

    Updated the code to pass all tests except for the minitest on line 16.

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,26 @@
+# 3/29/18
+require 'pry'
+
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+  return true if number == 0
+  i = 1
+  length = number.to_s.size
+  j = (number / i) % 10
+  k = (number % 10 ** length) / 10 ** (length - 1)
+
+  if length == 1 && j == number
+    return true
+  elsif length > 1
+    (length / 2).times do
+      i *= 10
+      length -= 1
+      if j == k
+        return true
+      else
+        return false
+      end
+    end
+  end
 end
